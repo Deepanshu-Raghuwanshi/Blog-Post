f3f7c0857b0b3f4023ffb02510fa28c434747d41
Deepanshu-Raghuwanshi <dipanshuraghuwanshio@gmail.com>
2025-01-30 01:57:21 +0530

    user uri

--- FE/my-app/src/pages/Home.jsx
+++ FE/my-app/src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
 
   const userName = localStorage.getItem("name");
 
-  console.log(userName, "userName");
+  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
   useEffect(() => {
     fetchPosts();
@@ -19,7 +19,7 @@ const Home = () => {
 
   const fetchPosts = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/api/posts");
+      const response = await axios.get(`${API_URL}/api/posts`);
       setPosts(response.data);
     } catch (error) {
       console.error("Error fetching posts:", error);
